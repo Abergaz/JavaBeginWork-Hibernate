829bb954044dc11a8ff217155540b7ec20f4da23
Abergaz <zagreba@gmail.com>
2019-12-02 14:12:07 +0300

    Основы Hibernate - 11. Добавление и обновление - lesson_11 и lesson_11h

---
+++ Hibernate/lesson_11h/11_HibernateSource_h/src/main/java/ru/javabegin/training/hibernate/entity/Author.java
@@ -0,0 +1,29 @@
+package ru.javabegin.training.hibernate.entity;
+
+import lombok.*;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@RequiredArgsConstructor
+@DynamicUpdate
+@DynamicInsert
+public class Author implements Serializable{
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY) // генерация ID через Autoincrement в MySQL
+    private long id;
+
+    @NonNull
+    private String name;
+
+    @Column(name = "second_name")
+    private String secondName;
+
+}
