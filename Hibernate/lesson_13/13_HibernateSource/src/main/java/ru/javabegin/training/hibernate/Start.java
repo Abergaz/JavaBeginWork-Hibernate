a7e9a5d662a8110ebebe376ae20d67896f992277
Abergaz <zagreba@gmail.com>
2019-12-02 15:17:21 +0300

    Основы Hibernate - 13. Выборка полей - lesson_13

---
+++ Hibernate/lesson_13/13_HibernateSource/src/main/java/ru/javabegin/training/hibernate/Start.java
@@ -0,0 +1,18 @@
+package ru.javabegin.training.hibernate;
+
+import org.jboss.logging.Logger;
+
+public class Start {
+
+    private static final Logger LOG = Logger.getLogger( AuthorHelper.class.getName() );
+
+
+    public static void main(String[] args) {
+
+        new AuthorHelper().getAuthorList();
+
+        HibernateUtil.getSessionFactory().close(); // закрываем фабрику, иначе программа останется в "зависнутом состоянии"
+
+    }
+
+}
