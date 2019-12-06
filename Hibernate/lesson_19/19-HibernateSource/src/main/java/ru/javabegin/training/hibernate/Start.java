f79894d68f092218045fc5a463985bc27080d3e7
Abergaz <zagreba@gmail.com>
2019-12-06 14:22:28 +0300

    Основы Hibernate - 19. Декомпозиция - lesson19

---
+++ Hibernate/lesson_19/19-HibernateSource/src/main/java/ru/javabegin/training/hibernate/Start.java
@@ -0,0 +1,29 @@
+package ru.javabegin.training.hibernate;
+
+import org.jboss.logging.Logger;
+import ru.javabegin.training.hibernate.entity.Book;
+
+import java.util.List;
+
+public class Start {
+
+    private static final Logger LOG = Logger.getLogger( AuthorHelper.class.getName() );
+
+
+    public static void main(String[] args) {
+
+//        new AuthorHelper().getAuthor(202);
+//        List<Author> list = new AuthorHelper().getAuthorList();
+//        List<Book> list = new BookHelper().getBookList();
+//        new AuthorHelper().getAuthor(1L).g
+
+
+        new AuthorHelper().update("test22");
+
+
+        HibernateUtil.getSessionFactory().close(); // закрываем фабрику, иначе программа останется в "зависнутом состоянии"
+
+
+    }
+
+}
